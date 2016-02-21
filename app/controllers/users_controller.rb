5ded23eca6eea8a1849e94d56981967f03309d20
mary_june <lily.daisy0619@gmail.com>
2016-02-21 15:30:06 +0000

    add followings/followers

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,7 @@ class UsersController < ApplicationController
 before_action :logged_in_user, only:  [:update]
 before_action :correct_user, only:[:edit, :update]
   
-  def show # 追加
+  def show
     @user = User.find(params[:id])
     @microposts = @user.microposts.order(created_at: :desc)
   end
@@ -43,17 +43,8 @@ before_action :correct_user, only:[:edit, :update]
                                  :password_confirmation)
   end
   
-  def logged_in_user
-    unless logged_in?
-      flash[:danger]= "Please log in."
-      store_location
-      redirect_to login_url
-    end  
-  end 
-
   def correct_user
     @user = User.find(params[:id])
     redirect_to(root_url) unless @user == current_user
   end
-
 end
