51a1b5d26eb5935b4bbd53b9c64681736783b8f4
mary_june <lily.daisy0619@gmail.com>
2016-03-10 16:15:12 +0000

    Gemfile update for heroku

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160220105837) do
+ActiveRecord::Schema.define(version: 20160310150934) do
 
   create_table "microposts", force: :cascade do |t|
     t.integer  "user_id"
@@ -43,6 +43,7 @@ ActiveRecord::Schema.define(version: 20160220105837) do
     t.text     "profile"
     t.string   "area"
     t.string   "url"
+    t.string   "avatar"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
